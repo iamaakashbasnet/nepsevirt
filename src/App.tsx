9d8abdea4b20432d0813766212d1b6670fa7e758
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-07-17 14:19:13 +0545

    chore: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <>
-      <div>App</div>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="test" element={<h1>Test Page</h1>} />
+      <Route path="*" element={<h1>Not Found 404</h1>} />
+    </Routes>
   );
 };
 
-export default App;
+const WrappedApp = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+export default WrappedApp;
