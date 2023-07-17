9ea3d6c3e833917e9bfc624386fea79488d4cf28
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-07-17 10:11:13 +0545

    chore: add v1->data app to django settings

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # API v1
+    'v1.data'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
