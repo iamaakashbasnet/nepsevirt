ac0dbd97e06a4e5a64a68eac73325a71a9a61d35
JavaIsNotHard <jibeshshrestha00@gmail.com>
2023-07-17 08:38:02 +0545

    refactor: add function to scrape; close #1

--- v1/data/scraper.py
+++ v1/data/scraper.py
@@ -2,29 +2,31 @@ import json
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://merolagani.com/LatestMarket.aspx')
-bs = BeautifulSoup(html.read(), 'lxml')
-namelist = bs.find('div', {'id': 'ctl00_ContentPlaceHolder1_LiveTrading'})
+def get_live_data():
+    html = urlopen('https://merolagani.com/LatestMarket.aspx')
+    bs = BeautifulSoup(html.read(), 'lxml')
+    namelist = bs.find('div', {'id': 'ctl00_ContentPlaceHolder1_LiveTrading'})
 
-childtag = namelist.find('table')
-childchildtag = childtag.find('tbody')
-gettrtag = childchildtag.find_all('tr')
+    childtag = namelist.find('table')
+    childchildtag = childtag.find('tbody')
+    gettrtag = childchildtag.find_all('tr')
 
-data = {}
-for i, elements in enumerate(gettrtag, 1):
-    tdtag = elements.find_all('td')
-    row_data = [td.text.strip() for td in tdtag if td.text.strip()]
-    if row_data:
-        data[f"{i}"] = row_data
+    data = {}
+    for i, elements in enumerate(gettrtag, 1):
+        tdtag = elements.find_all('td')
+        row_data = [td.text.strip() for td in tdtag if td.text.strip()]
+        if row_data:
+            data[f"{i}"] = row_data
 
-# Convert data to formatted JSON
-json_data = json.dumps(data, indent=4)
+    # Convert data to formatted JSON
+    json_data = json.dumps(data, indent=4)
 
-# Enclose the JSON data with curly braces
-formatted_json = "{" + "\n" + json_data + "\n" + "}"
+    # Enclose the JSON data with curly braces as json file
+    formatted_json = "{" + "\n" + json_data + "\n" + "}"
 
-print(json_data)
+    print(json_data)
 
-# Write JSON data to a file
-with open('data.json', 'w') as file:
-    file.write(json_data)
+    # Write JSON data to a file
+    with open('data.json', 'w') as file:
+        file.write(json_data)
+
