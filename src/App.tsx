a6656f741d9b4e1df9fea9568df2137ec466d9cc
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-07-18 19:47:51 +0545

    chore: folder structuring for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages';
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
-      <Route path="test" element={<h1>Test Page</h1>} />
+      <Route path="/" element={<Home />} />
       <Route path="*" element={<h1>Not Found 404</h1>} />
     </Routes>
   );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import logo from './../../assets/logo.png';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container">
+      <img src={logo} alt="Logo" />
+      <div>
+        <h1>NEPSE Virt</h1>
+        <p>Paper trading platform for NEPSE</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
