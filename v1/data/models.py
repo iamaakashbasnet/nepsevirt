b6ba339c2efabe5aeff8bcb9b6703a12da6a1441
JavaIsNotHard <jibeshshrestha00@gmail.com>
2023-07-20 15:50:47 +0545

    feat: add one to one relationship between data and name

--- v1/data/models.py
+++ v1/data/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
-# Create your models here.
-class StockData(models.Model):
+
+class StockName(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class StockData(models.Model):
+    name = models.OneToOneField(StockName, on_delete=models.CASCADE, primary_key=True,)
     open = models.FloatField()
     high = models.FloatField()
     low  = models.FloatField()
     close = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name.name}"
 
 
