866ae5e832739e5cc500717ab632a28cfea65e9a
JavaIsNotHard <jibeshshrestha00@gmail.com>
2023-07-20 15:16:50 +0545

    feat: add fields to data model

--- v1/data/models.py
+++ v1/data/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class StockData(models.Model):
     name = models.CharField(max_length=100)
+    open = models.FloatField()
+    high = models.FloatField()
+    low  = models.FloatField()
+    close = models.FloatField()
 
 
