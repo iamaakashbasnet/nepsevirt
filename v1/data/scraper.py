a17e52e880af171e3dbe07b96e07539215daccd4
JavaIsNotHard <jibeshshrestha00@gmail.com>
2023-07-21 11:29:06 +0545

    feat: modify scraper

--- v1/data/scraper.py
+++ v1/data/scraper.py
@@ -2,31 +2,29 @@ import json
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-def get_live_data():
-    html = urlopen('https://merolagani.com/LatestMarket.aspx')
-    bs = BeautifulSoup(html.read(), 'lxml')
-    namelist = bs.find('div', {'id': 'ctl00_ContentPlaceHolder1_LiveTrading'})
+html = urlopen('https://merolagani.com/LatestMarket.aspx')
+bs = BeautifulSoup(html.read(), 'lxml')
+namelist = bs.find('div', {'id': 'ctl00_ContentPlaceHolder1_LiveTrading'})
 
-    childtag = namelist.find('table')
-    childchildtag = childtag.find('tbody')
-    gettrtag = childchildtag.find_all('tr')
+childtag = namelist.find('table')
+childchildtag = childtag.find('tbody')
+gettrtag = childchildtag.find_all('tr')
 
-    data = {}
-    for i, elements in enumerate(gettrtag, 1):
-        tdtag = elements.find_all('td')
-        row_data = [td.text.strip() for td in tdtag if td.text.strip()]
-        if row_data:
-            data[f"{i}"] = row_data
+data = {}
+for elements in gettrtag:
+    tdtag = elements.find_all('td')
+    name = tdtag[0].text.strip()
+    row_data = {
+        'open': tdtag[1].text.strip(),
+        'high': tdtag[2].text.strip(),
+        'low': tdtag[3].text.strip(),
+        'close': tdtag[4].text.strip(),
+        'quantity': tdtag[5].text.strip()
+    }
+    data[name] = row_data
 
-    # Convert data to formatted JSON
-    json_data = json.dumps(data, indent=4)
+json_data = json.dumps(data, indent=4)
 
-    # Enclose the JSON data with curly braces as json file
-    formatted_json = "{" + "\n" + json_data + "\n" + "}"
-
-    print(json_data)
-
-    # Write JSON data to a file
-    with open('data.json', 'w') as file:
-        file.write(json_data)
-
+# Write JSON data to a file
+with open('data.json', 'w') as file:
+    file.write(json_data)
