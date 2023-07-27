7d478adb6593983a3c0ec13862cb59510958d763
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-07-27 10:00:20 +0545

    fix: typo in models

--- v1/data/models.py
+++ v1/data/models.py
@@ -6,8 +6,8 @@ from django.contrib.postgres.indexes import GinIndex
 class StockName(models.Model):
     name = models.CharField(max_length=100)
 
-    if settings.Debug is False: 
-        class Meta: 
+    if settings.DEBUG is False:
+        class Meta:
             ordering = ['name']
             indexes = [
                 GinIndex(name='gin_index', fields=['name'])
@@ -18,15 +18,15 @@ class StockName(models.Model):
 
 
 class StockData(models.Model):
-    name = models.OneToOneField(StockName, on_delete=models.CASCADE, unique=True)
+    name = models.OneToOneField(
+        StockName, on_delete=models.CASCADE, unique=True)
     open = models.FloatField()
     high = models.FloatField()
-    low  = models.FloatField()
+    low = models.FloatField()
     close = models.FloatField()
 
-    class Meta: 
+    class Meta:
         ordering = ['name']
 
     def __str__(self):
         return f"{self.name.name}"
-
