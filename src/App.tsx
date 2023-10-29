d94b921406fd1ce51aa99e455f537d54ed19b464
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-10-29 10:44:24 +0545

    refactor: background fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,9 @@ import Router from 'pages/router';
 
 const App: React.FC = () => {
   return (
-    <div className="bg-slate-50 dark:bg-gray-900 dark:text-white">
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 };
 
