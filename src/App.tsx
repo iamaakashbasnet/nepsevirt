7538414b70ff1507183404aa04ab1729d48266bf
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-10-30 12:07:32 +0545

    chore: react-query init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import Router from 'pages/router';
 import { FallbackLoading } from 'components';
 
 const App: React.FC = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <React.Suspense fallback={<FallbackLoading />}>
-        <Router />
-      </React.Suspense>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <React.Suspense fallback={<FallbackLoading />}>
+          <Router />
+        </React.Suspense>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
