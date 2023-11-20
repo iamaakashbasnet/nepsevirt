04508590e7aa4b294801e826423bb0526313ac64
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-11-20 14:26:58 +0545

    feat: chart using iframes nepsealpha

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,13 @@
 const Dashboard = () => {
-  return <h1 className="font-heading text-3xl">Dashboard</h1>;
+  return (
+    <section>
+      <iframe
+        src="https://www.nepsealpha.com/trading/chart"
+        height="550"
+        className="mx-auto w-full rounded-md border-2 border-gray-200 dark:border-gray-800"
+      ></iframe>
+    </section>
+  );
 };
 
 export default Dashboard;
