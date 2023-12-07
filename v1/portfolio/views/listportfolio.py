1c29a1dbe3de2e3df5bb133af7a88a858b3ae5d6
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-12-07 19:01:38 +0545

    feat: sending investment and current worth data in payload

--- v1/portfolio/views/listportfolio.py
+++ v1/portfolio/views/listportfolio.py
@@ -1,8 +1,11 @@
+from decimal import Decimal
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.response import Response
 
 from v1.portfolio.serializers.portfolio import PortfolioStockSerializer
 from v1.portfolio.models import Portfolio, PortfolioStock
+from v1.data.models import StockData
 
 
 class ListPortfolioStocks(generics.ListAPIView):
@@ -12,3 +15,27 @@ class ListPortfolioStocks(generics.ListAPIView):
     def get_queryset(self):
         return PortfolioStock.objects.filter(
             portfolio=Portfolio.objects.get(user=self.request.user))
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+
+        res = super().list(request=request).data
+
+        # Convert total_investment values to Decimal before summing
+        total_investment_values = [
+            Decimal(stock['total_investment']) for stock in serializer.data]
+
+        for _ in res:
+            _['current_worth'] = StockData.objects.get(
+                id=_['stock']['id']).ltp * _['total_quantity']
+
+        current_worth_values = [
+            stock['current_worth'] for stock in res]
+
+        modified_res = {
+            "portfolio_worth": sum(current_worth_values),
+            "portfolio_investment": sum(total_investment_values),
+            "portfolio": res
+        }
+        return Response(modified_res)
