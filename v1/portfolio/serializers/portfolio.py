3624372b4aa294f54c0ad392171e8304f0151eaf
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-12-29 16:37:42 +0545

    feat: complete portfolio data

--- v1/portfolio/serializers/portfolio.py
+++ v1/portfolio/serializers/portfolio.py
@@ -7,6 +7,10 @@ class PositionSerializer(serializers.ModelSerializer):
     stock = serializers.StringRelatedField()
     average_fill_price = serializers.DecimalField(
         max_digits=10, decimal_places=2, coerce_to_string=True)
+    investment_value = serializers.DecimalField(
+        max_digits=10, decimal_places=2, read_only=True)
+    current_value = serializers.DecimalField(
+        max_digits=10, decimal_places=2, read_only=True)
 
     class Meta:
         model = Position
--- v1/portfolio/views/listportfolio.py
+++ v1/portfolio/views/listportfolio.py
@@ -1,10 +1,12 @@
+from decimal import Decimal
+from django.db.models import F, ExpressionWrapper, fields
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.response import Response
 
 from v1.portfolio.serializers.portfolio import PositionSerializer
 from v1.portfolio.models import Portfolio, Position
-from v1.data.models import StockData
+from v1.data.models import StockData, StockName
 
 
 class ListPortfolioStocks(generics.ListAPIView):
@@ -12,5 +14,32 @@ class ListPortfolioStocks(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Position.objects.filter(
+        queryset = Position.objects.filter(
             portfolio=Portfolio.objects.get(user=self.request.user))
+
+        queryset = queryset.annotate(
+            investment_value=ExpressionWrapper(
+                F('quantity') * F('average_fill_price'),
+                output_field=fields.DecimalField()
+            ),
+            current_value=ExpressionWrapper(
+                F('quantity') * F('stock__stockdata__ltp'),
+                output_field=fields.DecimalField()
+            )
+        )
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        total_investment_value = sum(
+            item.investment_value for item in queryset)
+        total_current_value = sum(item.current_value for item in queryset)
+        serializer = self.get_serializer(queryset, many=True)
+
+        response_data = {'total_investment_value': total_investment_value,
+                         'total_current_value': total_current_value,
+                         'positions': serializer.data}
+
+        return Response(response_data)
