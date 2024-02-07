3f8ec4d2d3936a2e37ef991ee49ba702698cc107
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-02-07 19:29:36 +0545

    chore: fetch live data in every 2mins

--- src/pages/LiveMarket/LiveMarket.tsx
+++ src/pages/LiveMarket/LiveMarket.tsx
@@ -6,7 +6,7 @@ const LiveMarket = () => {
   const { data, isLoading } = useQuery({
     queryFn: () => fetchLiveData(),
     queryKey: ['live-data'],
-    refetchInterval: 60000,
+    refetchInterval: 120000,
   });
 
   return (
