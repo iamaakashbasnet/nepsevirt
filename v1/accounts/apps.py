d94dfed5461c90adbe311e7024217a8d7a627c2a
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-05-22 12:07:20 +0545

    fix: fund instance when user instance is created

--- v1/accounts/apps.py
+++ v1/accounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'v1.accounts'
+
+    def ready(self) -> None:
+        import v1.accounts.signals
