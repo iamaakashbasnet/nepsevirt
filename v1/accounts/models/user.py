6572bbb5bee06c810a09218cec2fcf6af62ebeaf
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-05-22 12:38:19 +0545

    refactor: super user are verified by default

--- v1/accounts/models/user.py
+++ v1/accounts/models/user.py
@@ -38,6 +38,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_verified', True)
 
         email = self.normalize_email(email)
         user = self.model(email=email, username=username,
