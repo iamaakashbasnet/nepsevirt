e1a9958a1c744030d9d62abcf965a335a866d347
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-06-21 10:51:57 +0545

    refactor: shift to unofficial api package

--- v1/officialapi/views/market.py
+++ v1/officialapi/views/market.py
@@ -2,18 +2,29 @@ import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
+from nepse import Nepse
 
-from v1.officialapi.bypasser import Nepse
+
+# TODO: Token expiry issue
+# from v1.officialapi.bypasser import Nepse
 
 
-# Base class for bypasser
 nepse = Nepse()
+nepse.setTLSVerification(False)
+
+
+# TODO: Token expiry issue
+# class IsMarketOpen(APIView):
+#     def get(self, request, *args, **kwargs):
+#         path = '/nepse-data/market-open'
+#         post_data = request.POST if request.method == 'POST' else None
+#         data, status_code = nepse.get_data(path, post_data)
+
+#         return Response(json.loads(data), status=status_code)
 
 
 class IsMarketOpen(APIView):
-    def get(self, request, *args, **kwargs):
-        path = '/nepse-data/market-open'
-        post_data = request.POST if request.method == 'POST' else None
-        data, status_code = nepse.get_data(path, post_data)
+    permission_classes = [permissions.IsAuthenticated]
 
-        return Response(json.loads(data), status=status_code)
+    def get(self, request, *args, **kwargs):
+        return Response(nepse.isNepseOpen())
